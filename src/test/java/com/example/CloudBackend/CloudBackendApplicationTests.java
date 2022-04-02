c6df9a6444c51e69184e588a8f50a592c6115069
VictorVel8 <victorvel801@gmail.com>
2022-04-02 17:40:29 +0300

    Project init

---
+++ src/test/java/com/example/CloudBackend/CloudBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CloudBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
